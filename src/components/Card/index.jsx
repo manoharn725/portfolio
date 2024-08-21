a0d4a8a210ac611af7d5fb3c9a61934be7025304
manoharn725 <manoharnmanu725@gmail.com>
2024-08-21 11:31:58 +0530

    card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,20 @@
+import "./index.scss";
+
+const Card = () => {
+  return (
+    <div className="card__container">
+      <div className="card__image"></div>
+      <div className="card__content">
+        <div className="card__content--icon"></div>
+        <div className="card__content--detailes">
+          <p className="card__content--title">Manohar</p>
+          <small className="card__content--descripation">
+            Loprem esdgfsd gsdfvsd dfvsfdv sdfs dvsdf fsegfdrgdr fgbfg gfh fgdrt
+            htfgsgsdf fsdfsv dfgv fvd sfsergvsd  srgfvdfvdf srgfvrtgbe dferferg vsrgvserw gfwergsr egssfv srgv t 
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card;
