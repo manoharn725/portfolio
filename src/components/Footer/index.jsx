fe9ebc25192adedcd0833ea7d6175e015ab0b29f
manoharn725 <manoharnmanu725@gmail.com>
2024-09-02 13:56:40 +0530

    social icons added to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,19 @@
-import './index.jsx';
+import './index.scss';
 
 const Footer = () => {
+    const socialIcons = ['github','instagram','linkedin','gmail'];
     return(
-        <div>Footer</div>
+        <div className="footer__wrap">
+             <div className="container footer__container">
+            <div className="footer__left">Manohar</div>
+            <div className="footer__right">
+                {socialIcons.map(item => (
+                    <i style={{backgroundImage:`var(--${item}-img)`}}></i>
+                ))}
+            </div>
+            </div>
+        </div>
+       
     )
 }
 export default Footer;
