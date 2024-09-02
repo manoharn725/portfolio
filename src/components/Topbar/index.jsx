efb4f7b5f50b3da6c68047ced4c894e5093f47a1
manoharn725 <manoharnmanu725@gmail.com>
2024-09-02 13:55:20 +0530

    added hamberger and otherthings

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,19 +1,31 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./index.scss";
 
 const Topbar = () => {
+  const [isHamburger, setIsHamburger] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+
+  const toHome = () => {
+    navigate('/');
+  };
+
+  const handleHamburger = () => {
+    setIsHamburger(!isHamburger);
+  };
+
+  const onNavigate = (event) => {
+    const path = event.currentTarget.getAttribute("data-path");
+    if (path) {
+      navigate(path);
+      setIsHamburger(false); // Close the hamburger menu when a navigation item is clicked
+    }
+  };
+
   const renderRightNav = () => {
     const navigation = [
-      { id: "home", label: "Home", path: "/", isDisabled: true },
-      { id: "about", label: "About", path: "/about/", isDisabled: true },
-      {
-        id: "education",
-        label: "Education",
-        path: "/education",
-        isDisabled: true,
-      },
+      { id: "home", label: "Home", path: "/" },
       {
         id: "design-system",
         label: "Design System",
@@ -26,37 +38,46 @@ const Topbar = () => {
       },
     ];
 
-    const onNavigate = (event) => {
-      const path = event.currentTarget.getAttribute("data-path");
-      if (path) {
-        navigate(path);
-      }
-    };
-
     return (
-      <div className="topbar__right">
-        {navigation.map(({ id, label, path, isDisabled }) => (
-          <div
-            key={id}
-            data-path={path}
-            className={`topbar__nav ${
-              location.pathname === path ? "topbar__nav--active" : ""
-            } ${isDisabled ? "topbar__nav--disabled" : ""}`}
+      <>
+        <div className={`topbar__right ${isHamburger ? "topbar__right--open" : ""}`}>
+          {navigation.map(({ id, label, path, isDisabled }) => (
+            <div
+              key={id}
+              data-path={path}
+              className={`topbar__nav ${
+                location.pathname === path ? "topbar__nav--active" : ""
+              } ${isDisabled ? "topbar__nav--disabled" : ""}`}
             onClick={onNavigate}
-          >
-            {label}
-          </div>
-        ))}
-      </div>
+            >
+              {label}
+            </div>
+          ))}
+        </div>
+        <div
+          className={`topbar__hamburger ${
+            isHamburger ? `topbar__hamburger--open` : ""
+          }`}
+          onClick={handleHamburger}
+        >
+          <div className="topbar__hamburger--line" />
+          <div className="topbar__hamburger--line" />
+          <div className="topbar__hamburger--line" />
+        </div>
+      </>
     );
   };
+
   return (
     <div className="topbar__wrap">
-      <div className="container topbar__container">
-        <div className="topbar__left">Manohar N</div>
+      <div className="container container__topbar">
+        <div className="topbar__left" onClick={toHome}>
+          Manohar N
+        </div>
         {renderRightNav()}
       </div>
     </div>
   );
 };
+
 export default Topbar;
