462c98b6cad4f1db4d84608db9513c9c26a14bb1
manoharn725 <manoharnmanu725@gmail.com>
2024-09-25 10:10:31 +0530

    updated mini project name

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -76,9 +76,9 @@ const Home = () => {
       projectIcon: "react-js",
       peojectUrl: "https://manohar-search-movie.netlify.app/",
       projectImage: "search-movies",
-      projectTitle: "Search English Movies",
+      projectTitle: "Search Movies",
       projectDescription: [
-        "Developed a Search English Movies website integrating Rapid API for real-time movie data",
+        "Developed a Search Movies website integrating Rapid API for real-time movie data",
         "Implemented efficient search functionality, enabling users to find detailed movie information seamlessly through a responsive and intuitive interface.",
       ],
     },
