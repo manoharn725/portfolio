4c295e07d45d5bcd7dfebc8a042aa490f4f7a5a7
manoharn725 <manoharnmanu725@gmail.com>
2024-10-19 20:05:33 +0530

    provider from redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/themeContext";
 import App from "./App";
+import { store } from "./redux/store";
 
 const el = document.getElementById("root");
 
@@ -10,10 +12,12 @@ const root = ReactDOM.createRoot(el);
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
