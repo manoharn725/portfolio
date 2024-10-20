85167b0440e5203e1583c693e0f0c7281abc1dbd
manoharn725 <manoharnmanu725@gmail.com>
2024-10-20 22:02:47 +0530

    skills set urls

--- src/api/skills.js
+++ src/api/skills.js
@@ -1,39 +1,39 @@
 export const skills = [
   {
     skill: "html-5",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
   },
   {
     skill: "css-3",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://developer.mozilla.org/en-US/docs/Web/CSS",
   },
   {
     skill: "scss",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://sass-lang.com/install/",
   },
   {
     skill: "javascript",
-    skillUrl: "https://www.javascript.com/",
+    skillUrl: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     skill: "typescript",
-    skillUrl: "https://www.javascript.com/",
+    skillUrl: "https://www.typescriptlang.org/",
   },
   {
     skill: "react-js",
-    skillUrl: "https://reactjs.org/",
+    skillUrl: "https://v3.vitejs.dev/guide/",
   },
   {
     skill: "redux-toolkit",
-    skillUrl: "https://redux.js.org/",
+    skillUrl: "https://redux.js.org/introduction/getting-started",
   },
   {
     skill: "sql-ssms",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://learn.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver16",
   },
   {
     skill: "bootstrap",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://getbootstrap.com/",
   },
   {
     skill: "git",
@@ -41,26 +41,26 @@ export const skills = [
   },
   {
     skill: "vs-code",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://code.visualstudio.com/",
   },
   {
     skill: "photoshop",
-    skillUrl: "https://whatwg.org/",
+    skillUrl: "https://www.adobe.com/in/products/photoshop/landpa.html?gclid=CjwKCAjw1NK4BhAwEiwAVUHPUCgfg3-1siApBTY5wGkRcGjIgphWBblBs7BGXbQm7dPmeWH0THGGxBoCdb0QAvD_BwE&sdid=GVTYXXRQ&mv=search&mv2=paidsearch&ef_id=CjwKCAjw1NK4BhAwEiwAVUHPUCgfg3-1siApBTY5wGkRcGjIgphWBblBs7BGXbQm7dPmeWH0THGGxBoCdb0QAvD_BwE:G:s&s_kwcid=AL!3085!3!645660845984!e!!g!!photoshop!16470706475!133281435039&gad_source=1",
   },
   // {
   //   skill: "next-js",
-  //   skillUrl: "https://whatwg.org/",
+  //   skillUrl: "https://nextjs.org/",
   // },
   // {
   //   skill: "mongodb",
-  //   skillUrl: "https://whatwg.org/",
+  //   skillUrl: "https://www.mongodb.com/try/download/community",
   // },
   // {
   //   skill: "postman",
-  //   skillUrl: "https://whatwg.org/",
+  //   skillUrl: "https://www.postman.com/",
   // },
   // {
   //   skill: "nodejs",
-  //   skillUrl: "https://whatwg.org/",
+  //   skillUrl: "https://nodejs.org/en",
   // },
 ];
