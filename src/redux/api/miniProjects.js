16b01ffc223558d9ecd3cc4a5ed531ab88f687f4
manoharn725 <manoharnmanu725@gmail.com>
2024-10-27 02:23:58 +0530

    shopping-store mini project

--- src/redux/api/miniProjects.js
+++ src/redux/api/miniProjects.js
@@ -1,6 +1,17 @@
 export const miniProjects = [
   {
     projectIcon: "react-js",
+    peojectUrl: "https://manohar-shopping-store.netlify.app/",
+    projectImage: "shopping-store",
+    projectTitle: "Shopping Store",
+    projectDescription: [
+      "Developed a responsive e-commerce app using Vite and React, integrating the Redux Toolkit for efficient state management.",
+      "Leveraged useSelector to access slice data, with asynchronous data fetching from FakeStoreAPI using createAsyncThunk for seamless data handling",
+      "Utilized dispatch to trigger actions and update state, ensuring a dynamic and interactive user experience.",
+    ],
+  },
+  {
+    projectIcon: "react-js",
     peojectUrl: "https://manohar-search-movie.netlify.app/",
     projectImage: "search-movies",
     projectTitle: "Search Movies",
