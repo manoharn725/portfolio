f50558731fd018018f5d4a3b08a47fcdfb4149a8
manoharn725 <manoharnmanu725@gmail.com>
2024-11-19 14:04:19 +0530

    updated ThemeProvider path in index.jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/themeContext";
+import { ThemeProvider } from "./context/Theme/ThemeContext";
 import App from "./App";
 import { store } from "./redux/store";
 
