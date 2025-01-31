ec3ded6f7ece1411b3b7194f5e27da84f0189db9
manoharn725 <manoharnmanu725@gmail.com>
2025-01-31 11:19:09 +0530

    temporarily hide

--- src/store/api/miniProjects.js
+++ src/store/api/miniProjects.js
@@ -10,17 +10,17 @@ export const miniProjects = [
       "By default, displayed movies related to the term 'Mad Max,' providing users with a curated initial experience.",
     ],
   },
-  {
-    projectIcon: "react-js",
-    peojectUrl: "https://manohar-shopping-store.netlify.app/",
-    projectImage: "shopping-store",
-    projectTitle: "Shopping Store",
-    projectDescription: [
-      "Developed a responsive e-commerce app using Vite and React, integrating the Redux Toolkit for efficient state management.",
-      "Leveraged useSelector to access slice data, with asynchronous data fetching from FakeStoreAPI using createAsyncThunk for seamless data handling",
-      "Utilized dispatch to trigger actions and update state, ensuring a dynamic and interactive user experience.",
-    ],
-  },
+  // {
+  //   projectIcon: "react-js",
+  //   peojectUrl: "https://manohar-shopping-store.netlify.app/",
+  //   projectImage: "shopping-store",
+  //   projectTitle: "Shopping Store",
+  //   projectDescription: [
+  //     "Developed a responsive e-commerce app using Vite and React, integrating the Redux Toolkit for efficient state management.",
+  //     "Leveraged useSelector to access slice data, with asynchronous data fetching from FakeStoreAPI using createAsyncThunk for seamless data handling",
+  //     "Utilized dispatch to trigger actions and update state, ensuring a dynamic and interactive user experience.",
+  //   ],
+  // },
   {
     projectIcon: "vite-js",
     peojectUrl: "https://manohar-mockapi-student-data.netlify.app/",
