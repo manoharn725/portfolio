aabd19b0aaf137fd0b50df39fa6feb0f3a5feb95
manoharn725 <manoharnmanu725@gmail.com>
2025-04-08 19:25:00 +0530

    implimented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Home from "./pages/Home";
-import DesignSystem from "./pages/DesignSystem";
 import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
-
 import PageNotFound from "./pages/PageNotFound";
+import CatLoader from "./components/CatLoader";
+
+//Lazy loading
+const DesignSystem = lazy(()=> import('./pages/DesignSystem'));
+
 const App = () => {
   return (
     <div className="app">
       <Topbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/design-system" element={<DesignSystem />} />
+        <Route path="/design-system" element={<Suspense fallback={<CatLoader />}><DesignSystem /></Suspense>} />
         {/* <Route path="/page-not-found" element={<PageNotFound />} /> */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
